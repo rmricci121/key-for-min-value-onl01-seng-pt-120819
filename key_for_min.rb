696541442813853031e456488ad92cc989183148
Robert M Ricci <rmricci121@gmail.com>
2019-12-16 19:12:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,12 @@
 
 def key_for_min_value(name_hash)
   lowest_value = 0
+  lowest_key = nil
   name_hash.each do |key, value|
     if value < lowest_value
       lowest_value = value
-      return key
+      lowest_key = key
     end
   end
+  lowest_key
 end
